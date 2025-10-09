b4bca46c8a9547b46e144bc34c51f3e709e3b314
Eyobel <eyucoli20@gmail.com>
2025-10-09 00:53:32 -0400

    banner added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Banner from './Components/Header/Banner/Banner'
 import Header from './Components/Header/Header'
 
 function App() {
   return (
     <>
     <Header/>
+    <Banner/>
     </>
   )
 }
---
+++ src/Components/Header/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import headshot from '../../../assets/images/profile_picture.jpg'
+import './Banner.css'
+
+function Banner() {
+    return (
+        <div className="Banner_outer_container">
+            <header classNameName="text-center py-5 bg-white shadow-sm">
+                <img 
+                src={headshot} 
+                alt="Profile Picture" className="rounded-circle border border-4 border-light shadow mb-4" width="340" height="340" />
+                <h1 className="fw-bold">Eyobel Gebremichael</h1>
+                <p className="fw-semibold">Software Developer | Washington, DC</p>
+            </header>
+        </div>
+    )
+}
+export default Banner
