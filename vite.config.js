fa289a02c37a82fa110eb0e3188a0dc1d60f84b3
Eyobel <eyucoli20@gmail.com>
2025-10-10 22:04:32 -0400

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// vite.config.js
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio_Web',    // 👈 Use '/' if deploying to root domain like eyobel.com
 })
+
+
